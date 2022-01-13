4b680942305aafc37d8829bf41b278bbddfcad2b
ParadoxFlame <shivammaji5818@gmail.com>
2022-01-12 16:48:59 -0800

    Hex & ActorsWorld Boilerplate

---
+++ JMCh21_6ActorsWorld/MailServer.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * TODO Write a one-sentence summary of your class here. TODO Follow it with
+ * additional details about its purpose, what abstraction it represents, and how
+ * to use it.
+ * 
+ * @author TODO Your Name
+ * @version TODO Date
+ * 
+ * @author Period - TODO Your Period
+ * @author Assignment - TODO Assignment Name
+ * 
+ * @author Sources - TODO list collaborators
+ */
+public class MailServer extends LinkedList<Message>
+{
+    private Set<Actor> actors;
+    
+    
+    // TODO complete class
+    
+    
+    // for testing purposes only
+    protected Set<Actor> getActors()
+    {
+        return actors;
+    }
+}
