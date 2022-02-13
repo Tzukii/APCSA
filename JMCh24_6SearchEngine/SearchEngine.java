ebec827635370366ed8add1c478cc8ad68f38f0a
ParadoxFlame <shivammaji5818@gmail.com>
2022-02-13 14:10:56 -0800

    JMCH24 & Search Engine BoilerPlate

---
+++ JMCh24_6SearchEngine/SearchEngine.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/**
+   TODO Write a one-sentence summary of your class here.
+   TODO Follow it with additional details about its purpose, what abstraction
+   it represents, and how to use it.
+
+   @author  TODO Your Name
+   @version TODO Date
+
+   @author Period - TODO Your Period
+   @author Assignment - JM24.6 - Search Engine
+
+   @author Sources - TODO list collaborators
+ */
+public class SearchEngine
+{
+    // Instance variable(s)
+    private String myURL; // holds the name for the "url" (file name)
+    private Map<String, List<String>> myIndex; // holds the word index
+
+    // Constructor(s)
+
+
+    // Methods
+
+
+    //*************************************************************
+    // For test purposes only
+    // not to be used in solution implementation
+    public Map<String, List<String>> getIndex()
+    {
+        return myIndex;
+    }
+}
