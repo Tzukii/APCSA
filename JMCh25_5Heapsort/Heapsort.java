52d3aa8861dee546bc26f95f0dca696031ba0fb5
ParadoxFlame <shivammaji5818@gmail.com>
2022-02-23 17:34:10 -0800

    JMCH25 Excercises, Heap of Trouble, & Heapsort Boilerplate

---
+++ JMCh25_5Heapsort/Heapsort.java
@@ -0,0 +1,28 @@
+/**
+   TODO Write a one-sentence summary of your class here.
+   TODO Follow it with additional details about its purpose, what abstraction
+   it represents, and how to use it.
+
+   @author  TODO Your Name
+   @version TODO Date
+
+   @author  Period - TODO Your Period
+   @author  Assignment - TODO Assignment Name
+
+   @author  Sources - TODO list collaborators
+ */
+public class Heapsort
+{
+    // Sorts a[0], ..., a[size-1] in ascending order
+    //   using the Mergesort algorithm
+    public static void sort(double[] a)
+    {
+        // TODO complete method
+    }
+
+    // Should be private - made public for testing
+    public static void reheapDown(double[] a, int i, int n)
+    {
+        // TODO complete method
+    }
+}
